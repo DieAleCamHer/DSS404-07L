26b23dc1f213455477b5419e67cffb436ff4f7fe
DieAleCamHer <dach04113@gmail.com>
2026-04-01 19:36:55 -0600

    guia8

---
+++ ciclo012025/guias/guia8/ejemplo3/php/conexion.php
@@ -0,0 +1,11 @@
+<?php
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "myapp";
+$con = new mysqli($host, $user, $password, $db);
+if (!$con->connect_error) {
+    $con->set_charset('utf8');
+} else {
+    echo "Se ha producido el siguiente error en la conexión: " . $con->connect_error;
+}
